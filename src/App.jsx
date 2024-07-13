6854b5a0fde1c90730935dc4e2f7d16d8a678233
Aman-Hussain-Shaikh <theamazingpeace@gmail.com>
2024-07-13 17:52:31 +0530

    First Commit of Infinite UI Project

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+
+function App() {
+
+
+  return (
+    <>
+
+      <div>
+
+        <Navbar />
+
+
+
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-center  ">
+    <h1 className="text-6xl font-black text-gray-900 hero-title">
+      Infinite UI
+    </h1>
+  </div>
+  )
+}
+
+export default Navbar
